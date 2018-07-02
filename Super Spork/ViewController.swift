9d5bbf85338fd96d56375ec59cddbe5e9f9004f8
amanda <anikkole796@gmail.com>
2018-07-02 16:10:32 -0700

    Initial Commit

---
+++ Super Spork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Super Spork
+//
+//  Created by Amanda Robinson on 7/2/18.
+//  Copyright © 2018 Amanda Robinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
